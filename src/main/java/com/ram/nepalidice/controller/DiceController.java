56e44883607c403df55b84d1e7636b789c93998b
ramchapagai <chapagairam@gmail.com>
2022-10-18 20:50:44 -0400

    (chore): minor refactoring

--- src/main/java/com/ram/nepalidice/controller/DiceController.java
+++ src/main/java/com/ram/nepalidice/controller/DiceController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,29 +18,30 @@ import static com.ram.nepalidice.util.DiceUtitlity.generateRandom;
 import static com.ram.nepalidice.util.DiceUtitlity.getDiceFace;
 
 @RestController
+@CrossOrigin()
 public class DiceController {
     private static final Logger LOGGER = LoggerFactory.getLogger(DiceController.class);
 
     @GetMapping("/roll-dice")
     public ResponseEntity<?> rollDice() {
 
-        Dice dice1 = new Dice.Builder().withFace(getDiceFace(generateRandom())).build();
-        Dice dice2 = new Dice.Builder().withFace(getDiceFace(generateRandom())).build();
-        Dice dice3 = new Dice.Builder().withFace(getDiceFace(generateRandom())).build();
-        Dice dice4 = new Dice.Builder().withFace(getDiceFace(generateRandom())).build();
-        Dice dice5 = new Dice.Builder().withFace(getDiceFace(generateRandom())).build();
-        Dice dice6 = new Dice.Builder().withFace(getDiceFace(generateRandom())).build();
+        Dice dice1 = new Dice.Builder().withDiceId("dice1").withFace(getDiceFace(generateRandom())).build();
+        Dice dice2 = new Dice.Builder().withDiceId("dice2").withFace(getDiceFace(generateRandom())).build();
+        Dice dice3 = new Dice.Builder().withDiceId("dice3").withFace(getDiceFace(generateRandom())).build();
+        Dice dice4 = new Dice.Builder().withDiceId("dice4").withFace(getDiceFace(generateRandom())).build();
+        Dice dice5 = new Dice.Builder().withDiceId("dice5").withFace(getDiceFace(generateRandom())).build();
+        Dice dice6 = new Dice.Builder().withDiceId("dice6").withFace(getDiceFace(generateRandom())).build();
 
         List<Dice> dices = List.of(dice1, dice2, dice3, dice4, dice5, dice6);
 
         //Create a map with DiceEnum key and number of faces of Dice as value
-        Map<DiceEnum, Long> diceMap = dices
+        Map<String, List<Dice>> diceMap = dices
                 .stream()
                 .collect(Collectors
-                        .groupingBy(Dice::getFace, Collectors.counting()));
+                        .groupingBy(Dice::getDiceId));
 
 
-        return new ResponseEntity<>(diceMap, HttpStatus.OK);
+        return new ResponseEntity<>(dices, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/ram/nepalidice/model/Dice.java
+++ src/main/java/com/ram/nepalidice/model/Dice.java
@@ -1,39 +1,63 @@
 package com.ram.nepalidice.model;
 
 public class Dice {
+    String diceId;
     DiceEnum face;
 
-    private Dice(Builder builder) {
-        setFace(builder.face);
+    public String getDiceId() {
+        return diceId;
     }
 
+    public void setDiceId(String diceId) {
+        this.diceId = diceId;
+    }
 
     public DiceEnum getFace() {
         return face;
+    }
+
+    @Override
+    public String toString() {
+        return "Dice{" +
+                "diceId=" + diceId +
+                ", face=" + face +
+                '}';
     }
 
     public void setFace(DiceEnum face) {
         this.face = face;
     }
 
-    @Override
-    public String toString() {
-        return "Dice{" +
-                "face=" + face +
-                '}';
+    private Dice(Builder builder) {
+        diceId = builder.diceId;
+        face = builder.face;
     }
+
 
     /**
      * {@code Dice} builder static inner class.
      */
     public static final class Builder {
+        private String diceId;
         private DiceEnum face;
 
         public Builder() {
         }
 
         public Builder(Dice copy) {
+            this.diceId = copy.getDiceId();
             this.face = copy.getFace();
+        }
+
+        /**
+         * Sets the {@code diceId} and returns a reference to this Builder enabling method chaining.
+         *
+         * @param diceId the {@code diceId} to set
+         * @return a reference to this Builder
+         */
+        public Builder withDiceId(String diceId) {
+            this.diceId = diceId;
+            return this;
         }
 
         /**
